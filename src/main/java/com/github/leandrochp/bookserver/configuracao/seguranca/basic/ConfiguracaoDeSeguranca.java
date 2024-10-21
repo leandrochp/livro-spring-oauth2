c14d14f3ee01c372e6b97464a50141e843397ea9
Leandro Pereira <leandrochp@gmail.com>
2024-10-20 21:00:22 -0300

    feat: cap 4

--- src/main/java/com/github/leandrochp/bookserver/configuracao/seguranca/basic/ConfiguracaoDeSeguranca.java
+++ src/main/java/com/github/leandrochp/bookserver/configuracao/seguranca/basic/ConfiguracaoDeSeguranca.java
@@ -1,12 +1,31 @@
 package com.github.leandrochp.bookserver.configuracao.seguranca.basic;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca {
+
+    @Configuration
+    @Order(1)
+    public static class ConfiguracaoParaAPI extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .antMatcher("/api/livros")
+                    .httpBasic()
+                    .and()
+                    .csrf().disable();
+        }
+    }
 
     @Configuration
     public static class ConfiguracaoParaUsuario extends WebSecurityConfigurerAdapter {
