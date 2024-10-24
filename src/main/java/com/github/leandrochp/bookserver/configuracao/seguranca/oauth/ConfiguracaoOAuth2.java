ed7c3de5b13c36cc5a95af77b7b2d5bb15162a86
Leandro Pereira <leandrochp@gmail.com>
2024-10-24 11:40:10 -0300

    :sparkles: feat: cap 7

--- src/main/java/com/github/leandrochp/bookserver/configuracao/seguranca/oauth/ConfiguracaoOAuth2.java
+++ src/main/java/com/github/leandrochp/bookserver/configuracao/seguranca/oauth/ConfiguracaoOAuth2.java
@@ -1,12 +1,15 @@
 package com.github.leandrochp.bookserver.configuracao.seguranca.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 @Configuration
@@ -38,12 +41,23 @@ public class ConfiguracaoOAuth2 {
     protected static class OAuth2AuthorizationServer
             extends AuthorizationServerConfigurerAdapter {
 
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
             clients.inMemory()
                     .withClient("cliente-app")
                     .secret("123456")
+                    .authorizedGrantTypes("password")
+                    .scopes("read", "write")
                     .resourceIds(RESOURCE_ID);
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
         }
     }
 }
+
